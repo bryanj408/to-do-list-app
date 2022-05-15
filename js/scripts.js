f4b225f882664986be52a9130ce135233655978b
bryanj408 <bryan.arndt52@gmail.com>
2022-05-15 10:46:59 -0700

    deleted js and added dblclick()

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,6 @@
 function newItem(){
 
 //1. Adding a new item to the list of items:
-   let li = document.createElement("li");
-   let inputValue = document.getElementById("input").value;
-   let text = document.createTextNode(inputValue);
-   li.appendChild(text);
-//rewrite in JQEURY
     let li = $('<li></li');
     let inputValue = $('#input').val();
     li.append(inputValue);
@@ -14,16 +9,17 @@ function newItem(){
    if (inputValue === '') {
      alert("You must write something!");
    } else {
-     let list = document.querySelector('#list');
-     list.appendChild(li);
+     $('#list').append(li);
    }
 
  //2. Crossing out an item from the list of items:
    function crossOut() {
- 		li.classList.toggle("strike");
+ 		li.toggleClass("strike");
  	}
-
- 	li.addEventListener("dblclick",crossOut);
+//added double click feature to cross/un-cross lines out
+ 	li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+  });
 
  //3(i). Adding the delete button "X":
    let crossOutButton = document.createElement("crossOutButton");

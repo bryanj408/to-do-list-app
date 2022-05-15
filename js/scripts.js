dc36cfb8b722ba259e20130e93039a36f3daea4f
bryanj408 <bryan.arndt52@gmail.com>
2022-05-15 10:59:23 -0700

    connected js and css files correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 
-function newItem(){
-
+function newItem() {
 //1. Adding a new item to the list of items:
     let li = $('<li></li');
     let inputValue = $('#input').val();
     li.append(inputValue);
 
    if (inputValue === '') {
-     alert('You must write something!'');
+     alert('You must write something!');
    } else {
      $('#list').append(li);
    }
@@ -27,10 +26,10 @@ function newItem(){
    li.append(crossOutButton);
    crossOutButton.on('click', deleteListItem);
 
- 	
+
  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
    function deleteListItem(){
- 		li.classList.add("delete")
+ 		li.addClass('delete');
  	}
  // 4. Reordering the items:
    $('#list').sortable();
